8485ac0c54db63098c5b0815ed9d1413468defe2
José Luiz <jose.iscosistemas@gmail.com>
2018-11-28 13:51:28 -0200

    -A
    change dbname

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ module.exports = {
   dialect: 'mysql',
   host: '127.0.0.1',
   port: '3306',
-  username: 'root',
-  password: 'Jymkatana_6985',
+  username: 'barber',
+  password: 'barber',
   database: 'gonodemodulo2',
   operatorAliases: false,
   define: {
